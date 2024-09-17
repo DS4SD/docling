8f1fd366eec6851802a6feed592af7b55fd3c78b
Peter Staar <taa@zurich.ibm.com>
2024-09-17 13:31:17 +0200

    feat: working on adding HF models for figure analysis (2)
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

---
+++ docling/models/figure_model.py
@@ -0,0 +1,13 @@
+
+
+class BaseFigureModel:
+
+    def __init__(self, config):
+        self.config = config
+
+class FlorenceFigureModel(BaseFigureModel):
+    pass
+
+class OneChartFigureModel(BaseFigureModel):
+    pass
+
