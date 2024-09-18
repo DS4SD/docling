78226ea7a22c46faa635fd7a8e3e712dc69df374
Peter Staar <taa@zurich.ibm.com>
2024-09-18 06:54:30 +0200

    reformatted the code
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- docling/models/figure_model.py
+++ docling/models/figure_model.py
@@ -1,13 +1,12 @@
-
-
 class BaseFigureModel:
 
     def __init__(self, config):
         self.config = config
 
+
 class FlorenceFigureModel(BaseFigureModel):
     pass
 
+
 class OneChartFigureModel(BaseFigureModel):
     pass
-
