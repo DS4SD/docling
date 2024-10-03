65ed754d37dbafb58fa98884105c4e6ed821cf9f
Maxim Lysak <mly@zurich.ibm.com>
2024-10-03 17:46:06 +0200

    Updated to docling-ibm-models v2.0.0
    Signed-off-by: Maxim Lysak <mly@zurich.ibm.com>

--- docling/document_converter.py
+++ docling/document_converter.py
@@ -67,7 +67,10 @@ class DocumentConverter:
         from huggingface_hub import snapshot_download
 
         download_path = snapshot_download(
-            repo_id="ds4sd/docling-models", force_download=force, local_dir=local_dir
+            repo_id="ds4sd/docling-models",
+            force_download=force,
+            local_dir=local_dir,
+            revision="v2.0.0",
         )
 
         return Path(download_path)
