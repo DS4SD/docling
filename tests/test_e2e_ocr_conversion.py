732bc6f515eaae0f858206d51dd5419fd83852cf
Christoph Auer <cau@zurich.ibm.com>
2024-11-27 12:31:04 +0100

    Fix OCR tests
    Signed-off-by: Christoph Auer <cau@zurich.ibm.com>

--- tests/test_e2e_ocr_conversion.py
+++ tests/test_e2e_ocr_conversion.py
@@ -78,6 +78,7 @@ def test_e2e_conversions():
                 input_path=pdf_path,
                 doc_result=doc_result,
                 generate=GENERATE_V1,
+                ocr_engine=ocr_options.kind,
                 fuzzy=True,
             )
 
@@ -85,5 +86,6 @@ def test_e2e_conversions():
                 input_path=pdf_path,
                 doc_result=doc_result,
                 generate=GENERATE_V2,
+                ocr_engine=ocr_options.kind,
                 fuzzy=True,
             )
