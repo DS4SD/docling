e125b9b24d41c122a74f9f68b3132ce096f07985
Nikos Livathinos <nli@zurich.ibm.com>
2024-12-08 18:32:08 +0100

    fix: main: Introduce format options for Image with the same pdf pipeline_options. Add RapidOcrOptions to the Union of ocr_options for PdfPipelineOptions
    Signed-off-by: Nikos Livathinos <nli@zurich.ibm.com>

--- docling/datamodel/pipeline_options.py
+++ docling/datamodel/pipeline_options.py
@@ -143,7 +143,7 @@ class PdfPipelineOptions(PipelineOptions):
 
     table_structure_options: TableStructureOptions = TableStructureOptions()
     ocr_options: Union[
-        EasyOcrOptions, TesseractCliOcrOptions, TesseractOcrOptions, OcrMacOptions
+        EasyOcrOptions, TesseractCliOcrOptions, TesseractOcrOptions, OcrMacOptions, RapidOcrOptions
     ] = Field(EasyOcrOptions(), discriminator="kind")
 
     images_scale: float = 1.0
