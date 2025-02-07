12df2ac2590582bb57e9ccdfe210012cf261a6d9
Michele Dolfi <dol@zurich.ibm.com>
2025-02-07 13:25:53 +0100

    use org--name in artifacts-path
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

--- docling/models/code_formula_model.py
+++ docling/models/code_formula_model.py
@@ -62,7 +62,7 @@ class CodeFormulaModel(BaseItemAndImageEnrichmentModel):
         Processes the given batch of elements and enriches them with predictions.
     """
 
-    _model_repo_folder = "CodeFormula"
+    _model_repo_folder = "ds4sd--CodeFormula"
     elements_batch_size = 5
     images_scale = 1.66  # = 120 dpi, aligned with training data resolution
     expansion_factor = 0.03
