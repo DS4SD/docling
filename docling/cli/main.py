291c91fe96827930109c90671f7829ebf37e08fd
Michele Dolfi <dol@zurich.ibm.com>
2025-02-11 12:51:36 +0100

    feat: Introduce the allow_remote_services option to allow remote connections while processing
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

--- docling/cli/main.py
+++ docling/cli/main.py
@@ -234,6 +234,12 @@ def convert(
         Optional[Path],
         typer.Option(..., help="If provided, the location of the model artifacts."),
     ] = None,
+    allow_remote_services: Annotated[
+        bool,
+        typer.Option(
+            ..., help="Must be enabled when using models connecting to remote services."
+        ),
+    ] = False,
     abort_on_error: Annotated[
         bool,
         typer.Option(
@@ -380,6 +386,7 @@ def convert(
 
         accelerator_options = AcceleratorOptions(num_threads=num_threads, device=device)
         pipeline_options = PdfPipelineOptions(
+            allow_remote_services=allow_remote_services,
             accelerator_options=accelerator_options,
             do_ocr=ocr,
             ocr_options=ocr_options,
